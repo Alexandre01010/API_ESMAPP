5995294adb3a77a57c2909b1236c04f2a07ecbf7
aleccunha <40190044@esmad.ipp.pt>
2022-01-04 18:13:46 +0000

    commit

--- controllers/resposta_controller.js
+++ controllers/resposta_controller.js
@@ -7,7 +7,7 @@ const Resposta = Model.Resposta;
 
 
 const listAll = (req, res) => {
-    Pergunta.findAll().then((clientesList) => {
+    Resposta.findAll().then((clientesList) => {
         if (clientesList.length > 0) {
             res.status(200).json(clientesList)
         } else {
@@ -19,4 +19,64 @@ const listAll = (req, res) => {
     })
 }
 
+const createResposta = (req, res) => {
+    Resposta.findAll({
+        where: {
+            texto: req.body.texto
+        }
+    }).then(resposta => {
+        if (resposta.length > 0) {
+            res.status(400).json({
+                message: "Essa resposta já existe"
+            })
+        } else {
+            Resposta.create({
+                texto: req.body.texto,
+                veracidade: req.body.veracidade,
+                perguntumId: req.params.idPergunta
+            }).then((resp) => {
+                res.status(201).json({
+                    message: "Resposta criada com sucesso"
+                })
+            }).catch(error => {
+                res.status(500).send("Internal Server Error")
+            })
+        }
+    })
+}
+
+const editResposta = (req, res) => {
+    Resposta.findAll({
+        where: {
+            id: req.params.idResposta
+        }
+    }).then((resposta) => {
+        if (resposta.length > 0) {
+            Resposta.update({
+                texto: req.body.texto,
+                veracidade: req.body.veracidade,
+                perguntumId: req.body.perguntaId
+            }, {
+                where: { id: req.params.idResposta }
+            }).then(data => {
+                res.status(201).json({
+                    message: "Resposta alterada com sucesso"
+                })
+            }).catch(error => {
+                res.status(500).send(error)
+            })
+        } else {
+            res.status(404).json({
+                message: "Resposta não encontrada"
+            })
+        }
+    }).catch((error) => {
+        res.status(500).json({
+            message: error
+        })
+    })
+}
+
 exports.listAll = listAll
+exports.createResposta = createResposta
+exports.editResposta = editResposta
--- routes/routes_resposta.js
+++ routes/routes_resposta.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const router = express.Router();
-const controller = require('../controllers/resposta_controller')
+const controllerRespostas = require('../controllers/resposta_controller')
 const { validationResult, body } = require('express-validator')
 
 
 router.get('/', function (req, res) {
-    controller.listAll(req, res);
+    controllerRespostas.listAll(req, res);
 })
+
+router.route('/:idPergunta').post([
+    body('texto').notEmpty(),
+    body('veracidade').notEmpty()
+], function (req, res) {
+    const errors = validationResult(req)
+    if (errors.isEmpty()) {
+        controllerRespostas.createResposta(req, res)
+    } else {
+        res.status(404).json({ errors: errors.array() })
+    }
+})
+
+router.route('/:idResposta').put(controllerRespostas.editResposta)
 
 
 module.exports = router
