a0e61da9bbf69c4022510d966c793ef020993368
aleccunha <40190044@esmad.ipp.pt>
2022-01-04 17:25:59 +0000

    commit

--- controllers/pergunta_controller.js
+++ controllers/pergunta_controller.js
@@ -19,4 +19,94 @@ const listAll = (req, res) => {
     })
 }
 
+// const createPergunta = (req, res) => {
+//     Pergunta.findAll({
+//         where:{
+//             texto: req.body.texto
+//         }
+//     }).then(per => {
+//         if(per > 0){
+//             res.status(400).json({
+//                 message: "Essa pergunta já existe"
+//             })
+//         }else{
+//             Pergunta.create({
+//                 texto: req.body.texto,
+//                 pontos: req.body.pontos,
+//                 exposicaoId: req.params.idExposicao
+//             }).then(pergunta => {
+//                 res.status(201).json({
+//                     message: "Pergunta criada com sucesso"
+//                 })
+//             }).catch(error => {
+//                 res.status(500).send(error)
+//             })
+//         }
+//     }).catch(error => {
+//         res.status(500).send(error)
+//     })
+// }
+
+const createPergunta = (req, res) => {
+    Pergunta.findAll({
+        where: {
+            texto: req.body.texto
+        }
+    }).then(perg => {
+        if (perg.length > 0) {
+            res.status(400).json({
+                message: "Essa pergunta já existe"
+            })
+        } else {
+            Pergunta.create({
+                texto: req.body.texto,
+                pontos: req.body.pontos,
+                exposicaoId: req.params.idExposicao
+            }).then((perg) => {
+                res.status(201).json({
+                    message: "Pergunta criada com sucesso"
+                })
+            }).catch(error => {
+                res.status(500).send("Internal Server Error")
+            })
+        }
+    })
+}
+
+const editPergunta = (req, res) => {
+    Pergunta.findAll({
+        where: {
+            id: req.params.idPergunta
+        }
+    }).then((pergunta) => {
+        if (pergunta.length > 0) {
+            Pergunta.update({
+                texto: req.body.texto,
+                pontos: req.body.pontos,
+                exposicaoId: req.body.idExposicao
+            }, {
+                where: { id: req.params.idPergunta }
+            }).then(data => {
+                res.status(201).json({
+                    message: "Pergunta alterada com sucesso"
+                })
+            }).catch(error => {
+                res.status(500).send(error)
+            })
+        } else {
+            res.status(404).json({
+                message: "Pergunta não encontrada"
+            })
+        }
+    }).catch((error) => {
+        res.status(500).json({
+            message: error
+        })
+    })
+}
+
+
+
 exports.listAll = listAll
+exports.createPergunta = createPergunta
+exports.editPergunta = editPergunta
--- routes/routes_pergunta.js
+++ routes/routes_pergunta.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const router = express.Router();
-const controller = require('../controllers/pergunta_controller')
+const controllerPerguntas = require('../controllers/pergunta_controller')
 const { validationResult, body } = require('express-validator')
 
 
 
 router.get('/', function (req, res) {
-    controller.listAll(req, res);
+    controllerPerguntas.listAll(req, res);
 })
+
+router.route('/:idExposicao').post([
+    body('texto').notEmpty(),
+    body('pontos').notEmpty()
+], function (req, res) {
+    const errors = validationResult(req)
+    if (errors.isEmpty()) {
+        controllerPerguntas.createPergunta(req, res)
+    } else {
+        res.status(404).json({ errors: errors.array() })
+    }
+})
+
+router.route('/:idPergunta').put(controllerPerguntas.editPergunta)
 
 
 module.exports = router
