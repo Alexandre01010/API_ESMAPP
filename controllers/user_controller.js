94cbed47cf51c47356525260e169d4d03f3a5b10
aleccunha <40190044@esmad.ipp.pt>
2022-01-05 01:38:20 +0000

    commit

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -4,6 +4,7 @@ const Model = require('../models/model_user');
 const { where } = require('sequelize/dist');
 const jwt = require('jsonwebtoken');
 const User = Model.User;
+const { Op } = require("sequelize");
 
 
 const listAll = (req, res) => {
@@ -19,4 +20,63 @@ const listAll = (req, res) => {
     })
 }
 
+const signUp = (req, res) => {
+    User.findOne({
+        where: {
+            [Op.or]: [
+                { email: req.body.email },
+                { username: req.body.username }
+            ]
+        }
+    }).then(user => {
+        if (user) {
+            res.status(409).json({
+                message: "Dados já existentes"
+            })
+        } else {
+            User.create({
+                username: req.body.username,
+                password: bcrypt.hashSync(req.body.password, 8),
+                email: req.body.email,
+                pontos: 0
+            }).then(userCreated => {
+                res.status(201).json({
+                    message: "Utilizador criado com sucesso"
+                })
+            }).catch(error => {
+                res.status(500).send(error)
+            })
+        }
+    }).catch(error => {
+        res.status(500).send(error)
+    })
+}
+
+const signIn = (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(user => {
+        if (user) {
+            const passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+            if (passwordIsValid) {
+                utilities.generateToken({ username: req.body.username }, token => {
+                    res.status(200).json(token)
+                })
+            } else {
+                res.status(400).json("Credenciais erradas")
+            }
+        }else{
+            res.status(404).json({
+                message: "Utilizador não encontrado"
+            })
+        }
+    }).catch(error => {
+        res.status(500).send(error)
+    })
+}
+
 exports.listAll = listAll
+exports.signUp = signUp
+exports.signIn = signIn
--- routes/routes_user.js
+++ routes/routes_user.js
@@ -1,20 +1,36 @@
 const express = require('express')
 const router = express.Router();
-const controller = require('../controllers/user_controller')
+const controllerUser = require('../controllers/user_controller')
 const { validationResult, body } = require('express-validator')
 
-// router.post('/login', [
-//     body('username').notEmpty().escape(),
-//     body('password').notEmpty().escape(),
+router.post('/registar', [
+    body('username').notEmpty().escape(),
+    body('email').notEmpty().isEmail(),
+    body('password').notEmpty().escape(),
 
-// ], function (req, res) {
-//     const errors = validationResult(req);
-//     if (errors.isEmpty()) {
-//         controller.login(req, res);
-//     } else {
-//         res.status(404).json({ errors: errors.array() })
-//     }
-// })
+], function (req, res) {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        controllerUser.signUp(req, res);
+    } else {
+        res.status(404).json({ errors: errors.array() })
+    }
+})
+
+router.post('/login', [
+    body('username').notEmpty().escape(),
+    body('password').notEmpty().escape(),
+
+], function (req, res) {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        controllerUser.signIn(req, res);
+    } else {
+        res.status(404).json({ errors: errors.array() })
+    }
+})
+
+
 
 
 
