d8f16cc6d877075a7ffc59c726c826f3e749a2c1
aleccunha <40190044@esmad.ipp.pt>
2022-01-20 17:10:30 +0000

    commit

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,7 +1,7 @@
 const utilities = require('../utilities/utilities')
 const bcrypt = require('bcrypt');
 const Model = require('../models/model_user');
-const { where } = require('sequelize/dist');
+//const { where } = require('sequelize/dist');
 const jwt = require('jsonwebtoken');
 const User = Model.User;
 const { Op } = require("sequelize");
