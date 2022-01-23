fcd821ff6634159dec28e8fce517dd8344e44fcc
fabianotorrao <40190082@esmad.ipp.pt>
2022-01-23 00:42:32 +0000

    autor

--- controllers/autor_controller.js
+++ controllers/autor_controller.js
@@ -50,7 +50,8 @@ const createAutor = (req, res) => {
         }else{
             Autor.create({
                 nome: req.body.nome,
-                biografia: req.body.biografia
+                biografia: req.body.biografia,
+                autorId:req.body.autorId
             }).then(autor => {
                 res.status(201).json({
                     message: "Autor criada com sucesso"
